fc8b75258ce636be2c64bdcb5648df879c04c9dc
MD Naem <mmnayeem408@gmail.com>
2023-05-02 16:46:39 +0600

    chef json data add

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!hello')
+    res.send('Hello World!')
 })
 
+
+
+
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
